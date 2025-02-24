fdbed7670731246b05352cc5df8f592addba9a4c
MaulanaKaka123 <maulanakakahw123@gmail.com>
2025-02-24 12:11:07 +0700

    Pertemuan 2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variables-local-global.js
@@ -0,0 +1,12 @@
+let globalVariable = "Maulana Kaka"
+
+function getMyIdentity(){
+    let localVariable = 2311104034
+
+    console.log(`Nama saya ${globalVariable} nim ${localVariable}`)
+}
+
+console.log(globalVariable)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variables.js
@@ -0,0 +1,21 @@
+var name = "Maulana Kaka"
+const nim = 2311104034
+
+console.log(name)
+
+console.log("Nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+console.log(`Nama saya adalah ${name}
+Nim saya adalah ${nim}`)
+
+var name = "Ayam"
+
+// let fullName = "Maulana Kaka"
+
+// const nim = 2311104031
+
+/*
+andnsdfnjnsndfn
+asdasd
+asdas fddgd
+*/
