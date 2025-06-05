7c5c171c251e3fc6784dcc3996411f0c92d24cde
MaulanaKaka123 <maulanakakahw123@gmail.com>
2025-06-05 21:39:41 +0700

    Modul 07

---
+++ 07_Parsing/TP_2311104034/index.js
@@ -0,0 +1,6 @@
+import dataMahasiswa2311104034 from './src/dataMahasiswa2311104034.js';
+import kuliahMahasiswa2311104034 from './src/kuliahMahasiswa2311104034.js';
+
+//uncomment satu baris untuk menjalankan program
+// dataMahasiswa2311104034.ReadJSON();
+kuliahMahasiswa2311104034.ReadJSON();
